26445f082030429e64b9baaf84b8260d02381147
Erran Carey <me@errancarey.com>
2014-04-27 11:53:04 -0500

    Create next_palindromic_integer.rb

---
+++ next_palindromic_integer.rb
@@ -0,0 +1,26 @@
+module PalindromeHelpers
+  module_function
+
+  def is_palindrome?(obj)
+    # NOTE: In Ruby 2.x String#chars already returns an array
+    chars = self.to_s.chars.to_a
+    chars == chars.reverse
+  end
+
+  def next_palindrome_int(num)
+    if is_palindrome?(num)
+      num.to_s+' is a palindrome'
+    else
+      num.to_s+' is not a palindrome'
+    end
+  end
+end
+
+p PalindromeHelpers.next_palindrome_int(12)
+# => "12 is not a palindrome"
+
+p PalindromeHelpers.next_palindrome_int(11)
+# => "11 is a palindrome"
+
+p PalindromeHelpers.next_palindrome_int(121)
+# => "121 is a palindrome"
