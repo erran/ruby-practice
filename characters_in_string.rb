9d99ba724822a292dea148dabe7840f0511a52c2
Erran Carey <me@errancarey.com>
2014-04-26 22:08:48 -0500

    Create characters_in_string.rb
    See http://thereq.com/q/138/ruby-software-interview-question/characters-in-strings

---
+++ characters_in_string.rb
@@ -0,0 +1,16 @@
+# Extracts the char
+def common_characters(str1, str2)
+  # NOTE: In Ruby 2.x chars returns an Array not an Enumerator like theReq's REPL
+  chars1 = str1.chars.to_a
+  chars2 = str2.chars.to_a
+
+  common_chars = (chars1 & chars2)
+  common_chars.uniq
+end
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+vowels = 'ueioa'
+
+chars = common_characters(alphabet, vowels)
+p chars
+# => ["a", "e", "i", "o", "u"]
